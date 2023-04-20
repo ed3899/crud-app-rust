d62eaae35b631c379d0364115a48e8657a8ff087
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-04-20 20:58:20 +0000

    added first route GET /employees

--- src/employees/routes.rs
+++ src/employees/routes.rs
@@ -1,6 +1,12 @@
-use crate::employees::{}
+use crate::employees::{Employee, Employees};
 use crate::error_handler::CustomError;
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use serde_json::json;
+
+#[get("/employees")]
+async fn find_all() -> Result<HttpResponse, CustomError> {
+  let employees = web::block(|| Employees::find_all()).await.unwrap();
+  Ok(HttpResponse::Ok().json(employees))
+}
 
 pub fn init_routes(config: &mut web::ServiceConfig) {}
