802969a33a0edb88a0d2acb1f543f8c91597c08a
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-04-20 22:15:20 +0000

    added initializer for db connection

--- src/db.rs
+++ src/db.rs
@@ -8,10 +8,23 @@ use std::env;
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
+embed_migrations!();
+
+lazy_static! {
+    static ref POOL: Pool = {
+        let db_url = env::var("DATABASE_URL").expect("Database url not set");
+        let manager = ConnectionManager::<PgConnection>::new(db_url);
+        Pool::new(manager).expect("Failed to created db pool")
+    };
+}
+
 pub fn init() {
-    todo!()
+    lazy_static::initialize(&POOL);
+    let conn = connection().expect("Failed to get db connection");
+    embedded_migrations::run(&conn).unwrap();
 }
 
 pub fn connection() -> Result<DbConnection, CustomError> {
-    todo!()
+    POOL.get()
+        .map_err(|e| CustomError::new(500, format!("Failed getting db connection: {}", e)))
 }
