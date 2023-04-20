c243bd383365bf49a6dee4242a14b3157381a053
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-04-20 20:33:46 +0000

    added basic func for db.rs

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,17 @@
-pub fn init() {
+use crate::error_handler::CustomError;
+use diesel::pg::PgConnection;
+use diesel::r2d2::ConnectionManager;
+use lazy_static::lazy_static;
+use r2d2;
+use std::env;
 
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+
+pub fn init() {
+    todo!()
+}
+
+pub fn connection() -> Result<DbConnection, CustomError> {
+    todo!()
 }
